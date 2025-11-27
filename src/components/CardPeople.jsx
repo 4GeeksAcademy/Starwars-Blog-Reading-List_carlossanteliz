c6a61378f40ed9e963f088465ceee1fd09f37c21
Charliekofg <charliekofg@hotmail.com>
2025-11-27 03:02:45 +0000

    Beginning CardPeople

---
+++ src/components/CardPeople.jsx
@@ -0,0 +1,11 @@
+import useGlobalReducer from "../hooks/useGlobalReducer";
+import { Link } from "react-router-dom";
+
+const CardPeople = ({people}) => {
+    const { store, dispatch } = useGlobalReducer()
+}
+
+
+return (
+    
+)
