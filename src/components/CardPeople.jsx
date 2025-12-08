4e265b11b65de9adb909d0b5e9c2e8244bd9040c
Charliekofg <charliekofg@hotmail.com>
2025-12-08 20:24:00 +0000

    tbd:make the carrousel

--- src/components/CardPeople.jsx
+++ src/components/CardPeople.jsx
@@ -1,55 +1,64 @@
 import useGlobalReducer from "../hooks/useGlobalReducer";
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import React from "react";
 
-const CardPeople = ({people}) => {
-    const { store, dispatch } = useGlobalReducer()
-    const favorite = store.favorites.some(f => f.uid == people.uid)
-    
-    const switchFavorite = () => {
-        
-        if (favorite) {
-            const index = store.favorites.findIndex(f => f.uid === people.uid);
-            dispatch({
-                type: "remove_favorite",
-                payload: { index }
-            });
+const CardPeople = ({ people }) => {
+  const { store, dispatch } = useGlobalReducer();
+  const favorite = store.favorites.some(f => f.uid === people.uid);
 
-        } else {
-            dispatch({
-                type: "add_favorite",
-                payload: {
-                    item: {
-                        name: people.name,
-                        uid: people.uid,
-                        type: "people"
-                    }
-                }
-            });
+  const switchFavorite = () => {
+    if (favorite) {
+      const index = store.favorites.findIndex(f => f.uid === people.uid);
+      dispatch({
+        type: "remove_favorite",
+        payload: { index }
+      });
+    } else {
+      dispatch({
+        type: "add_favorite",
+        payload: {
+          item: {
+            name: people.name,
+            uid: people.uid,
+            type: "people"
+          }
         }
-    };
+      });
+    }
+  };
 
+  return (
+    <div className="card m-3" style={{ width: "18rem" }}>
+      <img
+        src={`https://raw.githubusercontent.com/breatheco-de/swapi-images/master/public/images/people/${people.uid}.jpg`}
+        className="card-img-top"
+        alt={people.name}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{people.name}</h5>
+        <p className="card-text">
+          <strong>Gender:</strong> {people.gender}
+        </p>
+        <p className="card-text">
+          <strong>Hair Color:</strong> {people.hair_color}
+        </p>
+        <p className="card-text">
+          <strong>Height:</strong> {people.height}
+        </p>
 
+        <Link to={`/people/${people.uid}`} className="btn btn-primary me-2">
+          Learn more
+        </Link>
 
-return (
-    <div>
-        <div className="card" style={{width: "18rem"}}>
-            <img src={`https://raw.githubusercontent.com/breatheco-de/swapi-images/master/public/images/people/${people.uid}.jpg`} className="card-img-top" alt={people.name} />
-            <div className="card-body">
-                <h5 className="card-title">Name: {people.name}</h5>
-
-                <Link to={`/people/${people.uid}`} className="btn btn-primary">Go somewhere</Link>
-
-                <button onClick={switchFavorite}>
-                    {favorite ? "Unfavorite" : "Favorite"}
-                </button>
-
-            </div>
-
-        </div>
+        <button
+          onClick={switchFavorite}
+          className={`btn ${favorite ? "btn-danger" : "btn-outline-danger"}`}
+        >
+          {favorite ? "Unfavorite" : "Favorite"}
+        </button>
+      </div>
     </div>
-)
+  );
+};
 
-}
-
-export default CardPeople
+export default CardPeople;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,40 +4,33 @@ import { useEffect } from "react";
 import CardPeople from "../components/CardPeople.jsx";
 
 export const Home = () => {
+  const { store, dispatch } = useGlobalReducer();
 
-  const {store, dispatch} =useGlobalReducer()
+  async function people() {
+    const response = await fetch("https://www.swapi.tech/api/people/");
+    const data = await response.json();
+    const personajesBasicos = data.results;
 
-  async function people()  {
-	const response = await fetch("https://www.swapi.tech/api/people/")
-
-	const data = await response.json()
-	const personajesBasicos = data.results;
-	
-	dispatch({
-		type: "get_people",
-		payload: { people:personajesBasicos }
-	})
-
-
-
+    dispatch({
+      type: "get_people",
+      payload: { people: personajesBasicos }
+    });
   }
 
-
   useEffect(() => {
-	people()
-  },[])
+    people();
+  }, []);
 
-	
- 
- 
   return (
-		<div className="text-center mt-5">
-			<h1>Starwars!!</h1>
-			{store.people.map((value, index) => {
-				return (
-					<CardPeople key={index} people={value} />	
-				)
-			})}
-		</div>
-	);
-}; 
+    <div className="text-center mt-5">
+      <h1>Starwars!!</h1>
+
+      {/* Scrollable row container */}
+      <div className="d-flex flex-row overflow-auto">
+        {store.people.map((value, index) => (
+          <CardPeople key={index} people={value} />
+        ))}
+      </div>
+    </div>
+  );
+};
